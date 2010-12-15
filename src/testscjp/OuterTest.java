a5fd5305b4fab7dd9e3e1f3abb709a0e90fe1fe3
javediqubal.mohammad <javediqubal.mohammad@STPLT646.BLR.INDIA.CGINET>
2010-12-15 20:48:12 +0530

    Signed-off-by: javediqubal.mohammad <javediqubal.mohammad@STPLT646.BLR.INDIA.CGINET>

--- src/testscjp/OuterTest.java
+++ src/testscjp/OuterTest.java
@@ -5,6 +5,7 @@ public class OuterTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Airplane.Blackbox box=new Airplane().new Blackbox();
+		System.out.println("hi");
 		box.printvariables();
 	}
 
